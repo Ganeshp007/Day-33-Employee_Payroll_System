88203c0a45e4e867899eaaad535c4dcadc010e53
Ganeshp007 <potdart40.gp@gmail.com>
2022-06-09 20:57:40 +0530

    Added: UC1_CreateDataBaseUsing_Ado.net

---
+++ Day-33-Employee_Payroll_System/EmployeeRepo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_33_Employee_Payroll_System
+{
+    internal class EmployeeRepo
+    {
+        //path for Database Connection
+        public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Employee_payroll_33;Integrated Security=True;Connect Timeout=40;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+       
+        //method to establish connection
+        public void getConnection()
+        {
+            //Represents a connection to MS Sql Server Database
+            SqlConnection connection = new SqlConnection(connectionString);
+            Console.WriteLine("\n=> Connection Established successfully...");
+        }
+    }
+}
---
+++ Day-33-Employee_Payroll_System/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day_33_Employee_Payroll_System;
+
+Console.WriteLine("----- Welcome to Employee_Payroll Service using ADO Library -----\n");
+EmployeeRepo obj = new EmployeeRepo();
+obj.getConnection();
